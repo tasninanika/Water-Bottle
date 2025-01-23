31e49f3922ecfe41445ac823b9e3c9c65afde3dc
anikatasnin <jarintasnin09@gmail.com>
2025-01-23 14:30:28 +0600

    updated cart.jsx

--- src/Utilities/localStorage.js
+++ src/Utilities/localStorage.js
@@ -18,4 +18,10 @@ const addToLS = id=>{
     saveCartToLS(cart);
 }
 
+const removeFromLS = id =>{
+    const cart = getStoredCart();
+    // removing every id
+    const remaining = cart.filter(idx => idx !== id);
+}
+
 export { addToLS, getStoredCart }
