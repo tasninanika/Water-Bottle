5797a945633880a3dc6b600f7500ded6cea446ac
anikatasnin <jarintasnin09@gmail.com>
2025-01-23 12:23:31 +0600

    updated Bottles.jsx

--- src/Components/Bottle/Bottle.jsx
+++ src/Components/Bottle/Bottle.jsx
@@ -1,14 +1,14 @@
-
+import './Bottle.css';
 const Bottle = ({bottle}) => {
 
     const {name, img, price} = bottle;
     console.log(bottle);
 
     return (
-        <div>
+        <div className='bottle'>
             <h2>hello</h2>
             <p>Name: {name}</p>
-            <img src={img} alt="" />
+            <img src={img} alt="" className='img'/>
             <p>Price: ${price}</p>
         </div>
     );
