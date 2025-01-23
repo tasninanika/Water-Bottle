b6f507d3c5e3b84693fff2446ab812479a2e9dee
anikatasnin <jarintasnin09@gmail.com>
2025-01-23 14:15:08 +0600

    Added localStorage.js

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,8 +1,14 @@
+import './Cart.css';
 
 const Cart = ({cart}) => {
     return (
         <div>
             <p>Cart: {cart.length}</p>
+            <div className="cart-container">
+                {
+                    cart.map(bottle=> <img src={bottle.img}></img>)
+                }
+            </div>
         </div>
     );
 };
